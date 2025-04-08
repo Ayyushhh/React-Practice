6bf75527d0d28c99052bfa78d2e247ed0277eec6
Ayush Jain <ayushj820@gmail.com>
2025-04-09 00:46:11 +0530

    Practice part

---
+++ customReact/customreact.js
@@ -0,0 +1,26 @@
+function customerRender(reactElement, mainContainer){
+    /*
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href', reactElement.props.href)
+    domElement.setAttribute('target', reactElement.props.target)
+
+    mainContainer.appendChild(domElement);
+    */
+
+    const domElement = document.createElement(reactElement.type)
+    domElement
+}
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href:  'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+
+const mainContainer = document.querySelector('#root');
+
+customerRender(reactElement, mainContainer)
