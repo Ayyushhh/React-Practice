ec59131bdedf3cc09cccad7304b6a9cafd79c009
Ayush Jain <ayushj820@gmail.com>
2025-04-14 02:55:54 +0530

    Redux Todo

---
+++ redixToolkitTodo/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Addtodo from './components/Addtodo'
+import Todo from './components/Todo'
+
+function App() {
+  return (
+    <>
+      <h1>Hi</h1>
+      <Addtodo />
+      <Todo />
+    </>
+  )
+}
+
+export default App
+ 
